a8a73c3c1d5bd7bf71f86a2000df907714c03394
Tu Usuario de GitHub <erickgarcia2019@hotmail.com>
2023-02-06 16:17:00 -0600

    Aplicación de materialize para darle un mejor aspecto visual a los ejercicios

--- DSS/guia3PHP/guia3PHP/NotasArray/Notas.php
+++ DSS/guia3PHP/guia3PHP/NotasArray/Notas.php
@@ -48,17 +48,53 @@ $notas = array(
     )
 
     );
-    echo "Datos de los estudiantes: <br>";
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <title>Document</title>
+</head>
+<body class="container">
+    
+<?php
 
-    foreach ($notas as $alumnos){
-        echo "<br>";
+echo "<h1 style =","text-align:center",">Datos de los estudiantes </h1><br>";
+echo "<table class=","striped",">";
 
-            foreach($alumnos as $key => $value){
-                
-                echo $key, ": ", $value, "\n <br>";
+foreach ($notas as $alumnos){
 
-            }
+    echo "<thead >";
+        echo "<tr>";
+            echo "<th>Información</th>";
+            echo "<th>Descripción</th>";
+        echo "</tr>";
+    echo "</thead>";
 
-    }
+        foreach($alumnos as $key => $value){
+            echo "<tbodylass=","striped",">";
+            echo "<tr>";
+            echo "<td>", $key ,"</td>";
+            echo "<td>", $value ,"</td>";
+            echo "</tr>";
+        }
+}
+
+echo "</table>";
 
 ?>
+
+<br>
+
+<div class="container center-align">
+<a class="waves-effect waves-light btn" href="RecepcionDatos.html" >Regresar</a>
+</div>
+
+<br>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+</html>
+
--- DSS/guia3PHP/guia3PHP/TablasGuia3/TablasGuia3/Tablas.php
+++ DSS/guia3PHP/guia3PHP/TablasGuia3/TablasGuia3/Tablas.php
@@ -1,31 +1,61 @@
+<?php
+$tablas = $_POST['datosTabla'];
+$i = 0;
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
     <title>Document</title>
 </head>
-<body>
- 
-<?php
-$tablas = $_POST['datosTabla'];
-$i = 0;
+<body class="container">
 
-echo "<h1>Tabla del ",$tablas,"</h1>";
+
+<?php
+echo "<h1 style = ","text-align:center",">Tabla del ",$tablas,"</h1>";
+
+echo "<table class =","centered",">";
+
+echo "<thead>";
+echo "<tr>";
+echo "<th>Multiplicando</th>";
+echo "<th></th>";
+echo "<th>Multiplicador</th>";
+echo "<th>Producto</th>";
+echo "</tr>";
+echo "</thead>";
+
 
 while($i < 10){
+
+    
     ++$i;
     $resultado = ($tablas * $i);
-    echo $tablas, " x ", $i, " = ", $resultado, "<br>";  
-    continue;
+    echo "<tbody>";
+    echo "<tr>";
+    echo "<td>", $tablas ,"</td>";
+    echo "<td> x </td>";
+    echo "<td>", $i ,"</td>";
+    echo "<td>", $resultado ,"</td>";
+    echo "</tr>";
 
 }
 
 ?>
-
+</table>
 <br>
-<a href="IngresoDatos.html">Regresar</a>
+<br>
+<div class="container center-align">
+<a class="waves-effect waves-light btn" href="IngresoDatos.html" >Regresar</a>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
 </body>
 </html>
