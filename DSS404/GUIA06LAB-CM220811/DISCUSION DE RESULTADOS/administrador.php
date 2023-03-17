0b58b823d1890300375ab112d1c1b1b004ea394e
DiegoChevez <diegochevez144@gmail.com>
2023-03-16 23:50:42 -0600

    Ejercicio Complementario Guia 6 Terminado

--- DSS404/GUIA06LAB-CM220811/DISCUSION DE RESULTADOS/administrador.php
+++ DSS404/GUIA06LAB-CM220811/DISCUSION DE RESULTADOS/administrador.php
@@ -1,29 +1,25 @@
-<?php
-
-if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-  $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
-} else {
-  $ip_address = $_SERVER['REMOTE_ADDR'];
-}
-
-echo "La dirección IP completa desde la cual se accedió al archivo es: " . $ip_address;
-
-
-
-?>
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  </head>
-  <body class="container">
 
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap demo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
 
+<body class="container">
 
+  <?php
+  $file = fopen("log.txt", "r");
+  while (!feof($file)) {
+    $linea = fgets($file);
+    echo "<p>" . $linea . "</p>";
+  }
+  fclose($file);
+  ?>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  </body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+
 </html>
--- DSS404/GUIA06LAB-CM220811/DISCUSION DE RESULTADOS/random.php
+++ DSS404/GUIA06LAB-CM220811/DISCUSION DE RESULTADOS/random.php
@@ -1,3 +1,25 @@
+<?php
+$ip_address = $_SERVER['REMOTE_ADDR'];
+$nombre_script = $_SERVER['PHP_SELF'];
+$hora_actual = date('Y-m-d H:i:s');
+
+$text = "El script ". $nombre_script." se ejecuto a las ". $hora_actual . " desde la ip: ". $ip_address . ",";
+
+
+
+escritura($text);
+
+
+function escritura($text){
+  $archivo = "log.txt"; 
+  $datos = $text; 
+  $file = fopen($archivo, "a"); 
+  fwrite($file, $datos. PHP_EOL); 
+  fclose($file); 
+}
+
+
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -7,13 +29,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
   <body class="container">
-    
+
     <center>
-        <div class="btn-group btn-group-lg" role="group" aria-label="Large button group">
-            <a class="btn btn-outline-dark" href="administrador.php"> Administrador </a>
-            <a class="btn btn-outline-dark" href="random.php"> Random </a>
+        <h1>DOXEANDO...</h1>
+        <div class="spinner-border text-success" role="status">
+            <span class="visually-hidden">Loading...</span>
         </div>
+        <h1 style="color:#CCD4D9"><?php echo $text; ?></h1>
     </center>
+    
+    
+    
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
