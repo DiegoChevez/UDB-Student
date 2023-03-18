6d610a0c4cab797227eac5ddbe450d0c8b182474
DiegoChevez <diegochevez144@gmail.com>
2023-03-17 23:15:57 -0600

    Guia06 Terminada

--- DSS404/GUIA06LAB-CM220811/DISCUSION DE RESULTADOS/administrador.php
+++ DSS404/GUIA06LAB-CM220811/DISCUSION DE RESULTADOS/administrador.php
@@ -10,14 +10,30 @@
 
 <body class="container">
 
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Log</th>
+    </tr>
+  </thead>
+  <tbody>
   <?php
-  $file = fopen("log.txt", "r");
-  while (!feof($file)) {
-    $linea = fgets($file);
-    echo "<p>" . $linea . "</p>";
-  }
-  fclose($file);
-  ?>
+      $file = fopen("log.txt", "r");
+      $i = 0;
+      while (!feof($file)) {
+        $i++;
+        $linea = fgets($file);
+        echo '<tr>';
+        echo '<th scope="row">'. $i .'</th>';
+        echo '<td>'. $linea .'</td>';
+        echo '</tr>';
+      }
+      fclose($file);
+      ?>
+</table>
+
+  
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
